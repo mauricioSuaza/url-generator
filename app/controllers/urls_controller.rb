7f3ce1fe4978cd149cf2d6ac225b57a49822b8a5
maurcioVascarSolutions <mauricio.suaza@vascarsolutions.com>
2019-08-25 22:20:08 -0500

    added show method and view

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -2,7 +2,17 @@ class UrlsController < ApplicationController
   def index
     @url = Url.new
     @urls = Url.all
-    
+  end
+
+  def create
+    @url = Url.new(url_params)
+
+    if @url.save
+      redirect_to url_path(@url)
+    else
+      flash[:error] = @url.errors.full_messages
+      redirect_to root_path
+    end
   end
 
   def show
@@ -12,6 +22,6 @@ class UrlsController < ApplicationController
   private
 
     def url_params
-
+      params.require(:url).permit(:original)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'urls#index'
 
-  resources :urls, only: [:show, :index]
+  resources :urls, only: [:show, :index, :create]
 end
