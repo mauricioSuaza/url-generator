0a53f60517d54f5ec3ca7522d03e5f8f36aaf514
maurcioVascarSolutions <mauricio.suaza@vascarsolutions.com>
2019-08-26 03:07:52 -0500

    updated confingfor heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'urls#index'
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
 
+  root 'urls#index'
+  
   resources :urls, only: [:show, :index, :create]
 
   get '/:shortened_url', to: 'urls#redirect', as: 'manage_redirect'
